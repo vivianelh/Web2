8ee5726b0081245848a96bc7fea78e324fad2f23
vivianelh <51135707+vivianelh@users.noreply.github.com>
2020-10-18 18:45:34 -0300

    api

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next)=>{
+    res.status(200).send({
+        title: "Node Express API",
+        version: "0.0.1"
+    });
+});
+
+module.exports = router;
